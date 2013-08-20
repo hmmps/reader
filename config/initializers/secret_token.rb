d0a6499bca02a00bf17a8ce90d95d05bc740dfe6
Hampus Ahlm Brenander <hmmps@skenkonst.se>
2013-08-20 06:16:27 +0200

    Initial Commit. Rails4 with postgresql and RSpec/ Cucumber for testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reader::Application.config.secret_key_base = 'b719fe15b1c2e15aca44d4b0dc29b0a7d10dd8025899350c0c4794d5a549a91cb61b3b39834ed2addee86b81413025e5f1406f4ab11a265dd57f7e7c46c04e06'
